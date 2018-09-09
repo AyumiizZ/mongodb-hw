cc81c0766c806ba1fc17f0d9cdd4aaffb52a06a0
AyumiizZ <korrawitc@gmail.com>
2018-09-09 23:49:00 +0700

    :tada: finish Access control

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $('.delete-article').on('click', function (e) {
         $target = $(e.target);
-        console.log('fuck')
-        console.log($target.attr('data-id'));
         const id = $target.attr('data-id');
         $.ajax({
             type: 'DELETE',
